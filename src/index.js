d4c5cc74e0835ad58698f98e8393c4393616228a
Lucanms <mlucasm1231@gmail.com>
2025-12-15 19:31:04 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { crud } from "./crud.js";
-const [,, action, ...opts] = process.argv
+import parsedOpts from "./parseOpts.js";
+const [, , action, ...rawOpts] = process.argv;
 
-crud(action, opts)
+try {
+  const opts = parsedOpts(rawOpts);
+  crud(action, opts);
+} catch (err) {
+  console.error(err.message);
+  // process.exit(1);
+}
